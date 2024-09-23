6597da2afd61815ad3f77db67a267248d68b0a88
Unityell <103531085+Unityell@users.noreply.github.com>
2024-09-23 18:08:42 +0300

    wip

--- Assets/Scripts/Systems/Localize.cs
+++ Assets/Scripts/Systems/Localize.cs
@@ -8,12 +8,23 @@ public class Localize : MonoBehaviour
     [SerializeField] TextMeshProUGUI Text;
     [SerializeField] List<Localization> LangSettings;
 
+    private void OnEnable() => YandexGame.GetDataEvent += Localization;
+    private void OnDisable() => YandexGame.GetDataEvent -= Localization;
+
     void OnValidate()
     {
-        Text = GetComponent<TextMeshProUGUI>();
+        if(!Text) Text = GetComponent<TextMeshProUGUI>();
     }
 
     void Start()
+    {
+        if(YandexGame.SDKEnabled)
+        {
+            Localization();
+        }
+    }
+
+    void Localization()
     {
         Text.text = LangSettings.Find(x => x.Name == YandexGame.EnvironmentData.language).Phrase;
     }
